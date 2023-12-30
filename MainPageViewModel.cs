59148d787dcc63409e6b430d68c0217f17d891e3
SelminBiop <simbeliric@gmail.com>
2023-12-30 14:45:43 -0500

    Logique de jeu sans victoire pour toutes les cases. Pas tres DRY

--- MainPageViewModel.cs
+++ MainPageViewModel.cs
@@ -10,6 +10,12 @@ namespace tictactroll
 		O
 	}
 
+    public enum TourDuJoueur 
+    {
+        PremierJoueur,
+        DeuxiemeJoueur
+    }
+
     public sealed partial class MainPageViewModel : ObservableObject
     {
         [ObservableProperty]
@@ -23,47 +29,118 @@ namespace tictactroll
 
         [ObservableProperty]
         ValeursPossible _cinquiemeCaseValeur = ValeursPossible.Vide;
+
+        [ObservableProperty]
+        ICommand _quatriemeCaseTouchee;
+
+        [ObservableProperty]
+        ValeursPossible _quatriemeCaseValeur = ValeursPossible.Vide;
+
+        [ObservableProperty]
+        ICommand _troisiemeCaseTouchee;
+
+        [ObservableProperty]
+        ValeursPossible _troisiemeCaseValeur = ValeursPossible.Vide;
+
+        [ObservableProperty]
+        ICommand _deuxiemeCaseTouchee;
+
+        [ObservableProperty]
+        ValeursPossible _deuxiemeCaseValeur = ValeursPossible.Vide;
+
+        [ObservableProperty]
+        ICommand _sixiemeCaseTouchee;
+
+        [ObservableProperty]
+        ValeursPossible _sixiemeCaseValeur = ValeursPossible.Vide;
+
+        [ObservableProperty]
+        ICommand _septiemeCaseTouchee;
+
+        [ObservableProperty]
+        ValeursPossible _septiemeCaseValeur = ValeursPossible.Vide;
+
+        [ObservableProperty]
+        ICommand _huitiemeCaseTouchee;
+
+        [ObservableProperty]
+        ValeursPossible _huitiemeCaseValeur = ValeursPossible.Vide;
+
+
+        [ObservableProperty]
+        ICommand _neuviemeCaseTouchee;
+
+        [ObservableProperty]
+        ValeursPossible _neuviemeCaseValeur = ValeursPossible.Vide;
+
+        private TourDuJoueur _joueurCourant = TourDuJoueur.PremierJoueur;
         public MainPageViewModel()
         {
             _premiereCaseTouchee = new Command(SurPremiereCaseTouchee);
+            _deuxiemeCaseTouchee = new Command(SurDeuxiemeCaseTouchee);
+            _troisiemeCaseTouchee = new Command(SurTroisiemeCaseTouchee);
+            _quatriemeCaseTouchee = new Command(SurQuatriemeCaseTouchee);
             _cinquiemeCaseTouchee = new Command(SurCinquiemeCaseTouchee);
+            _sixiemeCaseTouchee = new Command(SurSixiemeCaseTouchee);
+            _septiemeCaseTouchee = new Command(SurSeptiemeCaseTouchee);
+            _huitiemeCaseTouchee = new Command(SurHuitiemeCaseTouchee);
+            _neuviemeCaseTouchee = new Command(SurNeuviemeCaseTouchee);
         }        
         private void SurPremiereCaseTouchee(object obj)
         {
-			if (PremiereCaseValeur is ValeursPossible.Vide)
-			{
-                PremiereCaseValeur = ValeursPossible.X;
-				return;
-            }
-            if (PremiereCaseValeur is ValeursPossible.X)
-            {
-                PremiereCaseValeur = ValeursPossible.O;
-				return;
-            }
-            if (PremiereCaseValeur is ValeursPossible.O)
-            {
-                PremiereCaseValeur = ValeursPossible.Vide;
-				return;
-            }
+            SurCaseTouchee(valeurDeLaCaseToucheeModification: nouvelleValeur => PremiereCaseValeur = nouvelleValeur, valeurDeLaCase: PremiereCaseValeur);
         }
         private void SurCinquiemeCaseTouchee(object obj)
         {
-            if (CinquiemeCaseValeur is ValeursPossible.Vide)
+            SurCaseTouchee(valeurDeLaCaseToucheeModification: nouvelleValeur => CinquiemeCaseValeur = nouvelleValeur, valeurDeLaCase: CinquiemeCaseValeur);
+        }
+              
+        private void SurDeuxiemeCaseTouchee(object obj)
+        {     
+            SurCaseTouchee(valeurDeLaCaseToucheeModification: nouvelleValeur => DeuxiemeCaseValeur = nouvelleValeur, valeurDeLaCase: DeuxiemeCaseValeur);
+        }
+        private void SurTroisiemeCaseTouchee(object obj) 
+        {
+            SurCaseTouchee(valeurDeLaCaseToucheeModification: nouvelleValeur => TroisiemeCaseValeur = nouvelleValeur, valeurDeLaCase: TroisiemeCaseValeur);
+        }
+        private void SurQuatriemeCaseTouchee(object obj)
+        {
+            SurCaseTouchee(valeurDeLaCaseToucheeModification: nouvelleValeur => QuatriemeCaseValeur = nouvelleValeur, valeurDeLaCase: QuatriemeCaseValeur);
+        }
+        private void SurSixiemeCaseTouchee(object obj)
+        {
+            SurCaseTouchee(valeurDeLaCaseToucheeModification: nouvelleValeur => SixiemeCaseValeur = nouvelleValeur, valeurDeLaCase: SixiemeCaseValeur);
+        }
+        private void SurSeptiemeCaseTouchee(object obj)
+        {
+            SurCaseTouchee(valeurDeLaCaseToucheeModification: nouvelleValeur => SeptiemeCaseValeur = nouvelleValeur, valeurDeLaCase: SeptiemeCaseValeur);
+        }
+        private void SurHuitiemeCaseTouchee(object obj)
+        {
+            SurCaseTouchee(valeurDeLaCaseToucheeModification: nouvelleValeur => HuitiemeCaseValeur = nouvelleValeur, valeurDeLaCase: HuitiemeCaseValeur);
+        }
+        private void SurNeuviemeCaseTouchee(object obj) 
+        {
+            SurCaseTouchee(valeurDeLaCaseToucheeModification: nouvelleValeur => NeuviemeCaseValeur = nouvelleValeur, valeurDeLaCase: NeuviemeCaseValeur);
+        }
+
+        private void SurCaseTouchee(Action<ValeursPossible> valeurDeLaCaseToucheeModification, ValeursPossible valeurDeLaCase) 
+        {
+            if (valeurDeLaCase is not ValeursPossible.Vide) 
             {
-                CinquiemeCaseValeur = ValeursPossible.X;
                 return;
             }
-            if (CinquiemeCaseValeur is ValeursPossible.X)
+
+            if (_joueurCourant is TourDuJoueur.PremierJoueur) 
             {
-                CinquiemeCaseValeur = ValeursPossible.O;
-                return;
-            }
-            if (CinquiemeCaseValeur is ValeursPossible.O)
+                valeurDeLaCaseToucheeModification(ValeursPossible.X);
+                _joueurCourant = TourDuJoueur.DeuxiemeJoueur;
+            } 
+            else 
             {
-                CinquiemeCaseValeur = ValeursPossible.Vide;
-                return;
+                valeurDeLaCaseToucheeModification(ValeursPossible.O);
+                _joueurCourant = TourDuJoueur.PremierJoueur;
             }
         }
     }
 }
-
